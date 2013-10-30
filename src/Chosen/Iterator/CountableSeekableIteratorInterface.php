33b6b02b110620c4151edc75b5e1a0ad1d31772f
Whizark <contact@whizark.com>
2013-10-30 15:48:28 +0900

    Iterator: add CountableSeekableIteratorInterface

---
+++ src/Chosen/Iterator/CountableSeekableIteratorInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Chosen\Iterator;
+
+use SeekableIterator;
+
+/**
+ * Interface CountableSeekableIteratorInterface
+ *
+ * @package Chosen\Iterator
+ * @author  Whizark
+ *
+ * @codeCoverageIgnore
+ */
+interface CountableSeekableIteratorInterface extends CountableIteratorInterface, SeekableIterator
+{
+}
